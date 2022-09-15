c8a67decbaf1d67fe82d4e39456a7f1c72801157
Vadimistar <vadimistar@ya.ru>
2022-09-15 18:14:13 +0700

    { and } handling in repl

--- cmd/wallrepl/main.go
+++ cmd/wallrepl/main.go
@@ -12,13 +12,32 @@ func main() {
 }
 
 var eval = wall.NewEvaluator()
+var depth = 0
+var buff = make([]byte, 0)
 
 func repl() {
-	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("> ")
+	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadBytes('\n')
 	text = text[:len(text)-1]
-	tokens, err := wall.ScanTokens("<repl>", text)
+	for _, ch := range text {
+		if ch == '{' {
+			depth++
+		}
+		if ch == '}' {
+			depth--
+		}
+	}
+	if depth < 0 {
+		panic("extra closing parenthesis")
+	}
+	if depth > 0 {
+		buff = append(buff, text...)
+		buff = append(buff, '\n')
+		repl()
+	}
+	tokens, err := wall.ScanTokens("<repl>", append(buff, text...))
+	buff = make([]byte, 0)
 	if err != nil {
 		panic(err)
 	}
